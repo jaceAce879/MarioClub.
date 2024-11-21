4c465a35157cd294d8564385acedff930db386ee
jaceAce879 <pauljnrpopoola@gmail.com>
2024-11-21 08:58:31 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+//Variables
+// variables are containers to hold data.
+var maxHealth = 1000;
+
+console.log(maxHealth);
+const pi = 3.44;
+
+health=45
+numofWeapons='i have changed'
+
+let number;
+
+if(number > 10){
+    console.log(number)
+}
+
+for(let i)
